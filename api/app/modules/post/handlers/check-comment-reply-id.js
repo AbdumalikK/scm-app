05d3467240af0fac0ef1ea4aa135ec44c47f2e5b
astrocoderr <alishersulimov@gmail.com>
2024-06-10 14:46:58 +0500

    enhacement: changed names

---
+++ api/app/modules/post/handlers/check-comment-reply-id.js
@@ -0,0 +1,47 @@
+import { Post } from '../models'
+
+import logger from '../../../utils/logs/logger'
+
+
+export default () => async (id, ctx, next) => {
+    try{
+        const post = await Post.findOne({
+            active: true, 
+            deletedAt: { $eq: null }, 
+            comment: { 
+                $elemMatch: {
+                    active: true,
+                    deletedAt: { $eq: null },
+                    reply: {
+                        $elemMatch: {
+                            _id: id,
+                            active: true,
+                            deletedAt: { $eq: null }
+                        }
+                    }
+                } 
+            }
+        })
+
+        if(!post){
+            ctx.status = 400
+            return ctx.body = {
+                success: false,
+                message: `Comment reply with id=${id} not found`,
+                data: null
+            }
+        }
+    }catch(ex){
+        logger.error(`Error. ${ex.status} ${ex.message}`)
+        ctx.status = 400
+        return ctx.body = {
+            success: false,
+            message: `Internal error`,
+            data: null
+        }
+    }
+
+    ctx.state.replyId = id
+
+    await next()
+}
